5fe2cb251fa3313c0ed0b2130708c9ed1c690bac
FundeVaibhav7787 <119277651+FundeVaibhav7787@users.noreply.github.com>
2022-11-28 10:13:53 +0530

    TODO TASK

---
+++ models.py
@@ -0,0 +1,19 @@
+from app import db
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    created_at = db.Column(db.Date)
+
+    def __repr__(self):
+        return self.text
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "created_at": str(self.created_at.strftime('%d-%m-%Y'))
+        }
